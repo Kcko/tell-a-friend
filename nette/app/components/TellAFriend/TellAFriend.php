f205fa9b609f8216289c5a9580e5d3202fc371cf
Jan Nový <jan.novy@lataupe.cz>
2014-08-24 11:11:23 +0200

    oprava formatovaní 3

--- nette/app/components/TellAFriend/TellAFriend.php
+++ nette/app/components/TellAFriend/TellAFriend.php
@@ -24,46 +24,44 @@ class TellAFriend extends Nette\Application\UI\Control {
 		$form = new Nette\Application\UI\Form;
 		$form->setAction('?');
 		
-		$form
-			->addText('mailto', 'Mail to')
+		$form->addText('mailto', 'Mail to')
 			->setAttribute('placeholder', 'od koho (e-mail)')
 			->setRequired('Zadejte prosím platný e-mail')
 			->setType('email')
 			->addRule($form::EMAIL, 'Zadejte prosím platný e-mail');
 
-		$form
-			->addText('mailfrom', 'Mail from')
+		$form->addText('mailfrom', 'Mail from')
 			->setAttribute('placeholder', 'komu (e-mail)')
 			->setRequired('Zadejte prosím platný e-mail')
- 	      	->setType('email')
-            ->addRule($form::EMAIL, 'Zadejte prosím platný e-mail');
+			->setType('email')
+			->addRule($form::EMAIL, 'Zadejte prosím platný e-mail');
 
 		$form->addSubmit('send', 'odeslat e-mail');
-        $form->onSuccess[] = $this->sendEmail;
+		$form->onSuccess[] = $this->sendEmail;
 		return $form;
 	}
 
 	public function sendEmail($form)
-    {
-        $form = $form->getForm();
-        $values = $form->getValues();
-        
-        $template = new Nette\Templating\FileTemplate(__DIR__ . '/message.latte');
-        $template->registerFilter(new Nette\Latte\Engine);
-        $template->registerHelperLoader('Nette\Templating\Helpers::loader');
-        $template->mailfrom = $values->mailfrom;
+	{
+		$form = $form->getForm();
+		$values = $form->getValues();
 
-        $mail = new Message;
-        $mail->setFrom($values->mailfrom)
-        ->addTo($values->mailto)
-        ->setSubject('Upozornění na stránky www.automarkyzy.cz')
-        ->setHtmlBody($template);
+		$template = new Nette\Templating\FileTemplate(__DIR__ . '/message.latte');
+		$template->registerFilter(new Nette\Latte\Engine);
+		$template->registerHelperLoader('Nette\Templating\Helpers::loader');
+		$template->mailfrom = $values->mailfrom;
 
-        $mailer = new SendmailMailer;
-        $mailer->send($mail);
+		$mail = new Message;
+		$mail->setFrom($values->mailfrom)
+			->addTo($values->mailto)
+			->setSubject('Upozornění na stránky www.automarkyzy.cz')
+			->setHtmlBody($template);
 
-        $this->redrawControl('tellafriend');
-	    $this->send = TRUE;
-    }
+		$mailer = new SendmailMailer;
+		$mailer->send($mail);
+
+		$this->redrawControl('tellafriend');
+		$this->send = TRUE;
+	}
 
 }
